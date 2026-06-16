144831c2d994b37d18e558ada26fa2357d9954e5
Abimael Miranda <mirandaabimael57@gmail.com>
2026-06-16 08:58:56 -0300

    wip: making shell pub+sub asynchrounous

--- src/Fluence.Core/Services/Modules/ShellEventBus.cs
+++ src/Fluence.Core/Services/Modules/ShellEventBus.cs
@@ -1,45 +1,65 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using Fluence.Core.Abstractions.Modules;
 
 namespace Fluence.Core.Services.Modules;
 
 public sealed class ShellEventBus : IShellEventBus
 {
+    private readonly Lock _lock = new();
+    private readonly Action<Action>? _dispatch;
     private readonly Dictionary<Type, List<Action<IShellEvent>>> _handlers = new();
     private readonly Dictionary<object, Action<IShellEvent>> _wrappers = new();
+
+    public ShellEventBus() { }
+
+    public ShellEventBus(Action<Action> dispatch) { _dispatch = dispatch; }
 
     public void Publish(IShellEvent shellEvent)
     {
         var type = shellEvent.GetType();
-        if (!_handlers.TryGetValue(type, out var handlers))
-            return;
+        Action<IShellEvent>[]? snapshot;
+        lock (_lock)
+        {
+            if (!_handlers.TryGetValue(type, out var handlers))
+                return;
+            snapshot = handlers.ToArray();
+        }
 
-        foreach (var h in handlers.ToArray())
-            h(shellEvent);
+        if (_dispatch is not null)
+            foreach (var h in snapshot)
+                _dispatch(() => h(shellEvent));
+        else
+            foreach (var h in snapshot)
+                h(shellEvent);
     }
 
     public void Subscribe<TEvent>(Action<TEvent> handler) where TEvent : IShellEvent
     {
         var type = typeof(TEvent);
-        if (!_handlers.TryGetValue(type, out var list))
-            _handlers[type] = list = [];
-
         Action<IShellEvent> wrapper = e => handler((TEvent)e);
-        _wrappers[handler] = wrapper;
-        list.Add(wrapper);
+        lock (_lock)
+        {
+            if (!_handlers.TryGetValue(type, out var list))
+                _handlers[type] = list = [];
+            _wrappers[handler] = wrapper;
+            list.Add(wrapper);
+        }
     }
 
     public void Unsubscribe<TEvent>(Action<TEvent> handler) where TEvent : IShellEvent
     {
         var type = typeof(TEvent);
-        if (!_handlers.TryGetValue(type, out var list))
-            return;
-
-        if (_wrappers.TryGetValue(handler, out var wrapper))
+        lock (_lock)
         {
-            list.Remove(wrapper);
-            _wrappers.Remove(handler);
+            if (!_handlers.TryGetValue(type, out var list))
+                return;
+            if (_wrappers.TryGetValue(handler, out var wrapper))
+            {
+                list.Remove(wrapper);
+                _wrappers.Remove(handler);
+            }
         }
     }
 }
