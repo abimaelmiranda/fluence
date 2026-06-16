8afb5d7e61f31a36d63102ddb10ef0c76e319bf7
Abimael Miranda <mirandaabimael57@gmail.com>
2026-06-16 19:58:22 -0300

    feat: adding build profiles

--- src/Fluence.Desktop/Program.cs
+++ src/Fluence.Desktop/Program.cs
@@ -9,8 +9,18 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        AppDomain.CurrentDomain.AssemblyResolve += ResolveModuleAssembly;
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
+
+    private static System.Reflection.Assembly? ResolveModuleAssembly(object? sender, ResolveEventArgs e)
+    {
+        var name = new System.Reflection.AssemblyName(e.Name).Name + ".dll";
+        var path = System.IO.Path.Combine(AppContext.BaseDirectory, "modules", name);
+        return System.IO.File.Exists(path) ? System.Reflection.Assembly.LoadFrom(path) : null;
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
