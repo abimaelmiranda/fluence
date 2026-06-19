035024e1526d0f479f2f005d33501352e0afd919
Abimael Miranda <mirandaabimael57@gmail.com>
2026-06-18 22:40:30 -0300

    fix: fixing icon index with lazy loaded solutions

--- modules/Fluence.Modules.SolutionView/Views/Converters/SolutionTreeItemIconConverter.cs
+++ modules/Fluence.Modules.SolutionView/Views/Converters/SolutionTreeItemIconConverter.cs
@@ -20,7 +20,10 @@ public sealed class SolutionTreeItemIconConverter : IValueConverter
         return item.Kind switch
         {
             SolutionTreeNodeKind.Solution => MaterialIconTheme.Instance.GetFileIcon(item.Path ?? item.Name)!,
-            SolutionTreeNodeKind.SolutionFolder or SolutionTreeNodeKind.Folder => MaterialIconTheme.Instance.GetFolderIcon(item.Name)!,
+            SolutionTreeNodeKind.SolutionFolder or SolutionTreeNodeKind.Folder =>
+                MaterialIconTheme.Instance.GetFolderIcon(item.Name) ??
+                MaterialIconTheme.Instance.GetIconById("folder") ??
+                MaterialIconTheme.Instance.GetIconById("file")!,
             SolutionTreeNodeKind.Project => MaterialIconTheme.Instance.GetFileIcon(item.Path ?? item.Name)!,
             SolutionTreeNodeKind.Dependencies or SolutionTreeNodeKind.DependencyGroup => MaterialIconTheme.Instance.GetIconById("dependencies-update")!,
             SolutionTreeNodeKind.ProjectReference => MaterialIconTheme.Instance.GetFileIcon(item.ReferencedProjectPath ?? item.Name)!,
